5fc02e99f1e8d14bf1f600945199bc9ec4471f61
auba86 <audrius.bareisis@gmail.com>
2022-02-09 20:41:16 +0200

    Sixth Task Finished

--- Task6/task6.js
+++ Task6/task6.js
@@ -9,13 +9,33 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-    { id: '1', name: 'John Smith', age: 20 },
-    { id: '2', name: 'Ann Smith', age: 24 },
-    { id: '3', name: 'Tom Jones', age: 31 },
-    { id: '4', name: 'Rose Peterson', age: 17 },
-    { id: '5', name: 'Alex John', age: 25 },
-    { id: '6', name: 'Ronald Jones', age: 63 },
-    { id: '7', name: 'Elton Smith', age: 16 },
-    { id: '8', name: 'Simon Peterson', age: 30 },
-    { id: '9', name: 'Daniel Cane', age: 51 },
-  ];
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
+];
+
+function getUsersAverageAge(data) {
+  let sum = 0;
+  data.forEach(user => {
+  sum += user.age; 
+  });
+  console.log(`All users average age is: ${(sum / data.length).toFixed(1)}`);
+}
+
+getUsersAverageAge(users);
+
+function getUsersNames(data) {
+  const namesArray = [];
+  data.forEach((user) => {
+    namesArray.push(user.name);
+  });
+  return namesArray;
+}
+
+console.log(getUsersNames(users));
