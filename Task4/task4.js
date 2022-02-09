eeb4741e66d81a8ba266566c1db376175a05490e
auba86 <audrius.bareisis@gmail.com>
2022-02-09 20:09:31 +0200

    Fourth Task Finished

--- Task4/task4.js
+++ Task4/task4.js
@@ -9,4 +9,42 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const output = document.getElementById("output");
+
+window.onload = () => {
+  fetchCarsList();
+};
+
+function fetchCarsList() {
+  fetch("cars.json")
+    .then((res) => res.json())
+    .then((data) => {
+      createCarsCards(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+function createCarsCards(data) {
+  data.forEach((car) => {
+    const div = document.createElement("div");
+    const brand = document.createElement("h3");
+    brand.textContent = car.brand;
+    brand.style.fontSize = '26px';
+    div.append(brand);
+
+    const br = document.createElement('br');
+    div.append(br);
+
+    const ul = document.createElement("ul");
+    car.models.forEach((model) => {
+      
+      const li = document.createElement("li");
+      li.textContent = model;
+      ul.append(li);
+    });
+
+    div.append(ul);
+    output.append(div);
+    
+  });
+}
