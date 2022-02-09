a442271c5f447cd5996a1323428b2d9a1347dfd2
auba86 <audrius.bareisis@gmail.com>
2022-02-09 18:54:02 +0200

    Second Task Finished

--- Task2/task2.js
+++ Task2/task2.js
@@ -5,11 +5,11 @@ pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
 const button = document.getElementById("btn__element");
 
-const result = document.getElementById("btn__state");
+const output = document.getElementById("btn__state");
 
 let counter = 0;
 
 button.addEventListener("click", () => {
   counter++;
-  result.textContent = counter;
+  output.textContent = counter;
 });
