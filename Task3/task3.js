a818d6f8fbf37f63e26b1e41691e9e6e1f95dc06
auba86 <audrius.bareisis@gmail.com>
2022-02-09 19:30:29 +0200

    Third Task Finished

--- Task3/task3.js
+++ Task3/task3.js
@@ -10,4 +10,45 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+// const ENDPOINT = 'https://api.github.com/users';
+
+const showUsersBtn = document.getElementById('btn');
+const output = document.getElementById('output');
+const message = document.getElementById('message');
+
+showUsersBtn.addEventListener('click', () => {
+
+    message.remove();
+
+    fetch('https://api.github.com/users')
+    .then((res) => res.json())
+    .then((data) => {
+        drawUsersList(data);
+    })
+    .catch((err) => alert(err))
+});
+
+function drawUsersList(data) {
+    data.forEach(user => {
+        
+        const table = document.createElement('table');
+        output.append(table);
+
+        const tr = document.createElement('tr');
+        const tdLogin = document.createElement('td');
+        const tdUrl = document.createElement('td');
+
+        const img = document.createElement('img');
+        img.src = user.avatar_url;
+       
+        tdUrl.append(img);
+        tr.append(tdUrl);
+
+        tdLogin.textContent = `User login: ${user.login}`;
+
+        tr.append(tdLogin);
+        table.append(tr);        
+    });
+
+}
+
