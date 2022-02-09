ba7aba11bdbf3319192b9b2398689d2e4fbcbb96
auba86 <audrius.bareisis@gmail.com>
2022-02-09 21:19:22 +0200

    Ninth Task Finished

--- Task9/task9.js
+++ Task9/task9.js
@@ -6,3 +6,29 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie{
+
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+
+    wasExpensive() {
+        if(this.budget > 100_000_000) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    printAllData() {
+        console.log(`Movie title: ${this.title} \nDirector: ${this.director} \n${this.title} budget: ${this.budget} dollars`)
+    }
+}
+
+const movie = new Movie("Titanic", "James Cameron", 100000001);
+console.log(movie.wasExpensive());
+movie.printAllData();
