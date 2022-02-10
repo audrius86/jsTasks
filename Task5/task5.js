9fd8b338669e3e33db865adb425aa7319f3072ee
auba86 <audrius.bareisis@gmail.com>
2022-02-10 18:53:23 +0200

    + clean some empty lines

--- Task5/task5.js
+++ Task5/task5.js
@@ -9,31 +9,29 @@ atveju grąžins masyvą su "users", kurie yra pilnamečiai.
 -------------------------------------------------------------------------- */
 
 const users = [
-    { id: '1', name: 'John Smith', age: 20, hasDog: true },
-    { id: '2', name: 'Ann Smith', age: 24, hasDog: false },
-    { id: '3', name: 'Tom Jones', age: 31, hasDog: true },
-    { id: '4', name: 'Rose Peterson', age: 17, hasDog: false },
-    { id: '5', name: 'Alex John', age: 25, hasDog: true },
-    { id: '6', name: 'Ronald Jones', age: 63, hasDog: true },
-    { id: '7', name: 'Elton Smith', age: 16, hasDog: true },
-    { id: '8', name: 'Simon Peterson', age: 30, hasDog: false },
-    { id: '9', name: 'Daniel Cane', age: 51, hasDog: true },
-  ];
+  { id: "1", name: "John Smith", age: 20, hasDog: true },
+  { id: "2", name: "Ann Smith", age: 24, hasDog: false },
+  { id: "3", name: "Tom Jones", age: 31, hasDog: true },
+  { id: "4", name: "Rose Peterson", age: 17, hasDog: false },
+  { id: "5", name: "Alex John", age: 25, hasDog: true },
+  { id: "6", name: "Ronald Jones", age: 63, hasDog: true },
+  { id: "7", name: "Elton Smith", age: 16, hasDog: true },
+  { id: "8", name: "Simon Peterson", age: 30, hasDog: false },
+  { id: "9", name: "Daniel Cane", age: 51, hasDog: true },
+];
 
-  function filterDogOwners(data) {
-    let dogOwnersList = data.filter((value) => value.hasDog === true);
-    console.log(dogOwnersList);
+function filterDogOwners(data) {
+  let dogOwnersList = data.filter((value) => value.hasDog === true);
+  console.log(dogOwnersList);
+  //or
+  //console.log(data.filter((value) => value.hasDog === true));
+}
 
-    //or
-    
-    //console.log(data.filter((value) => value.hasDog === true));
-  }
+filterDogOwners(users);
 
-  filterDogOwners(users);
+function filterAdults(data) {
+  let adultsList = data.filter((value) => value.age >= 18);
+  console.log(adultsList);
+}
 
-  function filterAdults(data){
-    let adultsList = data.filter((value) => value.age >= 18);
-    console.log(adultsList);
-  }
-
-  filterAdults(users);
+filterAdults(users);
